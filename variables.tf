c1922fb2eac39334ccbb15ed92afe2529c9904fa
aliogorobinna <aliogor7509@yahoo.com>
2023-04-25 17:20:43 +0200

    vault with datasource

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "access_key" {
+  type    = string
+  default = "AKIAY5G7DRWECA6SGBKN"
+}
+
+variable "secret_key" {
+  type    = string
+  default = "Xtdu4UqSPyvOgDK6BnhPPNUslnoOjrHOG7MY2+ZF"
+}
+  variable "vault_addr" {
+    type = string
+    default = "http://127.0.0.1:8200"
+  }
+
+  variable "vault_token" {
+    type = string
+    default = "hvs.41vTGCUsvNqzFLTuEQnL6S5m"
+  }
